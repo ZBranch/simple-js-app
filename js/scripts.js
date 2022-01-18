0d99955195c5e2518d34c1d20a3e8e080ddbf5bd
ZBranch <zackwbranch@gmail.com>
2022-01-18 01:41:46 -0600

    Update scripts.js
    Each object represents a Pokemon's name, height, and types

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
-let pokemonList = [];
+let pokemonList =
+    [{name = "Bulbasaur", height = 0.7, types = ["Grass", "Poison"]},
+        {name = "Charizard", height = 1.7, types = ["Fire","Flying"]},
+            {name = "Squirtle", height = 0.5, types = "Water"},
+                  {name = "Pikachu", height = 0.4, types = "Electric"},
+                      {name = "Rattata", height = 0.3, types = "Normal"},
+                          {name = "Nidoking", height = 1.4, types = "Ground", "Poison"}];
