b9583abb9c0f03700ce49c1c55f5d9f76406cb2e
ZBranch <zackwbranch@gmail.com>
2022-01-20 07:48:14 -0600

    Update scripts.js
    created conditions for each Pokemon's height

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+<script>
+
 let pokemonList =
     [{name: "Bulbasaur", height: 0.7, types: ["Grass", "Poison"]},
         {name: "Charizard", height: 1.7, types: ["Fire","Flying"]},
@@ -5,3 +7,14 @@ let pokemonList =
                   {name: "Pikachu", height: 0.4, types: "Electric"},
                       {name: "Rattata", height: 0.3, types: "Normal"},
                           {name: "Nidoking", height: 1.4, types: "Ground", "Poison"}];
+
+let pokemon = {name: "Bulbasaur", height: 0.7};
+  if (pokemon.size > 1.5) {
+    console.log("This is a large Pokemon!")
+  } else if (pokemon.height > 0.5 && pokemon.height < 1.5) {
+    console.log("This is an average Pokemon");
+  } else {
+    console.log("This is a small Pokemon")
+  }
+
+</script>
