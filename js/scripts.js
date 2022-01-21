0173d87690ed533080bb28aea2c1cb923ed17565
ZBranch <zackwbranch@gmail.com>
2022-01-20 18:54:42 -0600

    Update scripts.js
    Removed the script tags that weren't necessary and put an array around Nidoking's  Types

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,10 @@
-<script>
-
 let pokemonList =
     [{name: "Bulbasaur", height: 0.7, types: ["Grass", "Poison"]},
         {name: "Charizard", height: 1.7, types: ["Fire","Flying"]},
             {name: "Squirtle", height: 0.5, types: "Water"},
                   {name: "Pikachu", height: 0.4, types: "Electric"},
                       {name: "Rattata", height: 0.3, types: "Normal"},
-                          {name: "Nidoking", height: 1.4, types: "Ground", "Poison"}];
+                          {name: "Nidoking", height: 1.4, types: ["Ground", "Poison"]}];
 
 let pokemon = {name: "Bulbasaur", height: 0.7};
   if (pokemon.size > 1.5) {
@@ -16,5 +14,3 @@ let pokemon = {name: "Bulbasaur", height: 0.7};
   } else {
     console.log("This is a small Pokemon")
   }
-
-</script>
